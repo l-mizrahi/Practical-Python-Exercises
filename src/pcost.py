b2a15bc6b6137954e05072cc4c18b555b535aacf
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-07-27 23:40:53 +0200

    refactor: ex1.32 portfolio_cost now uses csv reader to read files

--- src/pcost.py
+++ src/pcost.py
@@ -1,11 +1,14 @@
+import csv
+
+
 def portfolio_cost(filename):
     total_cost = 0
     with open(filename) as f:
-        next(f)
-        for line in f:
-            if not line:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            if not row:
                 raise ValueError
-            row = line.split(",")
             total_cost += int(row[1]) * float(row[2])
 
     return total_cost
