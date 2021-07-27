2c83ce4ebfa240e8a3c657a7e3fe1bbb9988020a
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-07-27 23:28:26 +0200

    fix: fix path to data files

--- tests/test_pcost.py
+++ tests/test_pcost.py
@@ -3,9 +3,9 @@ from src.pcost import portfolio_cost
 
 
 def test_portfolio_cost():
-    assert portfolio_cost("src/Data/portfolio.csv") == 44671.15
+    assert portfolio_cost("data/portfolio.csv") == 44671.15
 
 
 def test_portfolio_cost_missing_values():
     with pytest.raises(ValueError):
-        portfolio_cost("src/Data/missing.csv")
+        portfolio_cost("data/missing.csv")
