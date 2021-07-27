c149b1c75b93597545e4cb29a13e46f61a843cb5
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-07-27 23:29:57 +0200

    feat: ex1.27 add portfolio_cost function

--- src/pcost.py
+++ src/pcost.py
@@ -1,2 +1,11 @@
 def portfolio_cost(filename):
-    pass
+    total_cost = 0
+    with open(filename) as f:
+        next(f)
+        for line in f:
+            if not line:
+                raise ValueError
+            row = line.split(",")
+            total_cost += int(row[1]) * float(row[2])
+
+    return total_cost
