c603571923d7f49c7a4efa6b678e7793d827075d
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-07-28 16:26:20 +0200

    tests: ex2.16 add more tests for portfolio_cost for more file formats

--- tests/test_pcost.py
+++ tests/test_pcost.py
@@ -9,3 +9,12 @@ def test_portfolio_cost():
 def test_portfolio_cost_missing_values():
     with pytest.raises(ValueError):
         portfolio_cost("data/missing.csv")
+
+
+def test_portfolio_cost_different_headers():
+    assert portfolio_cost("data/portfoliodate.csv") == 44671.15
+
+
+def test_portfolio_cost_incorrect_format():
+    with pytest.raises(ValueError):
+        portfolio_cost("data/portfolio3.csv")
