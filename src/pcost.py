905e92c65ba733d274cbc628c379d446dc14d652
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-07-28 16:37:55 +0200

    feat: ex2.16 add support to portfolio_cost for more file formats

--- src/pcost.py
+++ src/pcost.py
@@ -5,10 +5,15 @@ def portfolio_cost(filename):
     total_cost = 0
     with open(filename) as f:
         rows = csv.reader(f)
-        next(rows)
+        headers = next(rows)
         for row in rows:
             if not row:
+                continue
+            try:
+                row = dict(zip(headers, row))
+                total_cost += int(row["shares"]) * float(row["price"])
+
+            except Exception:
                 raise ValueError
-            total_cost += int(row[1]) * float(row[2])
 
     return total_cost
