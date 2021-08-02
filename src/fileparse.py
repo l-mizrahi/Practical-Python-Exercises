bdbbb7a0596cae9c4e3c7c22deb0e7d4621caba3
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 13:34:41 +0200

    feat(ex3.3): add parse_csv function

--- src/fileparse.py
+++ src/fileparse.py
@@ -1,12 +1,24 @@
+import csv
 from pathlib import Path
 from src.report import PortDict
 from typing import List, Union
 
 
 def parse_csv(file_path: Union[str, Path]) -> List[PortDict]:
-    """[summary]
-
-    :param file_path: [description]
-    :type file_path: Union[str, Path]
     """
-    pass
+    Parse a csv file into a list.
+
+    :param file_path: Path to the csv file
+    :return: List of items in the file
+    """
+    records: List[PortDict] = []
+    with open(file_path) as f:
+        rows = csv.reader(f)
+        next(rows)
+        for name, shares, price in rows:
+            if not all([name, shares, price]):
+                continue
+            pdict = PortDict(name=name, shares=int(shares), price=float(price))
+            records.append(pdict)
+
+    return records
