dc56824e912f713372dc6be2f263f8d22640b339
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 14:48:57 +0200

    feat(ex3.6): add has_headers to parse_csv

--- src/fileparse.py
+++ src/fileparse.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-from typing import List, Union, Callable
+from typing import List, Tuple, Union, Callable, Dict
 
 
 def parse_csv(
@@ -15,18 +15,21 @@ def parse_csv(
     :param file_path: Path to the csv file
     :param select: Columns to select
     :param types: List of types to convert each column
-    :param has_headers: Indicator if file has headers
+    :param has_headers: Indicator if file has headers.
+                        If False, creates a list of tuples otherwise a list of dictionaries
     :return: List of items in the file
     """
     records: List[object] = []
+    record: Union[Dict, Tuple]
+    indices: List[int] = []
+
     with open(file_path) as f:
         rows = csv.reader(f)
-        headers = next(rows)
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
 
         for row in rows:
             if not row:
@@ -35,7 +38,11 @@ def parse_csv(
                 row = [row[index] for index in indices]
             if types:
                 row = [func(val) for func, val in zip(types, row)]
-            record = dict(zip(headers, row))
+
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
