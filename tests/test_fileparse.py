24cfc0b99f21e595be33479b911e905e3f45563d
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 13:52:22 +0200

    tests: fix test with wrong output

--- tests/test_fileparse.py
+++ tests/test_fileparse.py
@@ -5,12 +5,12 @@ from src.fileparse import parse_csv
 def test_parse_csv():
     portfolio = parse_csv(DATA_DIRECTORY / "portfolio.csv")
     expected = [
-        {"name": "AA", "shares": "100", "price": "32.2"},
-        {"name": "IBM", "shares": "50", "price": "91.1"},
+        {"name": "AA", "shares": "100", "price": "32.20"},
+        {"name": "IBM", "shares": "50", "price": "91.10"},
         {"name": "CAT", "shares": "150", "price": "83.44"},
         {"name": "MSFT", "shares": "200", "price": "51.23"},
         {"name": "GE", "shares": "95", "price": "40.37"},
-        {"name": "MSFT", "shares": "50", "price": "65.1"},
+        {"name": "MSFT", "shares": "50", "price": "65.10"},
         {"name": "IBM", "shares": "100", "price": "70.44"},
     ]
 
