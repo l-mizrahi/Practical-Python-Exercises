1d93de8a85523f8f65eb32a0156e62358f189fe7
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 18:08:19 +0200

    fix: remove parsing tests from test_pcost.py

--- tests/test_pcost.py
+++ tests/test_pcost.py
@@ -1,6 +1,5 @@
 from src.pcost import portfolio_cost
 from src import DATA_DIRECTORY
-import logging
 
 
 def test_portfolio_cost():
@@ -15,20 +14,6 @@ def test_portfolio_cost_missing_values(caplog):
     Tests if portfolio_cost raises a ValueError if there are blank lines in the file.
     """
     cost = portfolio_cost(DATA_DIRECTORY / "missing.csv")
-    expected_error = [
-        (
-            "root",
-            logging.ERROR,
-            "Could not process row number 4: {'name': 'MSFT', 'shares': '', 'price': '51.23'}",
-        ),
-        (
-            "root",
-            logging.ERROR,
-            "Could not process row number 7: {'name': 'IBM', 'shares': '', 'price': '70.44'}",
-        ),
-    ]
-
-    assert caplog.record_tuples == expected_error
     assert cost == 27381.15
 
 
@@ -41,16 +26,7 @@ def test_portfolio_cost_different_headers():
 
 def test_portfolio_cost_incorrect_format(caplog):
     """
-    Tests if portfolio_cost raises a ValueError if type conversion cannot be done.
+    Tests if portfolio_cost logs rows that cannot be converted.
     """
     cost = portfolio_cost(DATA_DIRECTORY / "portfolio3.csv")
-    expected_error = [
-        (
-            "root",
-            logging.ERROR,
-            "Could not process row number 2: {'name': 'HPQ', 'shares': '250', 'price': 'forty'}",
-        )
-    ]
-
-    assert caplog.record_tuples == expected_error
     assert cost == 9121.25
