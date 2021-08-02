2f77e27f09fc69cb98e0aada000b0f5a050ce686
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 18:50:22 +0200

    feat(ex3.17): parse_csv accepts path or file-like objects

--- src/fileparse.py
+++ src/fileparse.py
@@ -1,11 +1,11 @@
 import csv
 import logging
 from pathlib import Path
-from typing import Any, List, Tuple, Union, Callable, Dict, cast
+from typing import Any, List, TextIO, Tuple, Union, Callable, Dict, cast
 
 
 def parse_csv(
-    file_path: Union[str, Path],
+    file_path_or_buffer: Union[str, Path, TextIO],
     select: List[str] = None,
     types: List[Callable] = None,
     has_headers: bool = True,
@@ -15,7 +15,7 @@ def parse_csv(
     """
     Parse a csv file into a list.
 
-    :param file_path: Path to the csv file
+    :param file_path_or_buffer: Path to the csv file or file-like object
     :param select: Columns to select
     :param types: List of types to convert each column
     :param has_headers: Indicator if file has headers.
@@ -31,34 +31,36 @@ def parse_csv(
     indices: List[int] = []
     start: int = 0
 
-    with open(file_path) as f:
-        rows = csv.reader(f, delimiter=delimiter)
-        if has_headers:
-            headers = next(rows)
-            start = 1
-            records = cast(List[Dict], records)
-            if select:
-                indices = [headers.index(colname) for colname in select]
-                headers = select
-        else:
-            records = cast(List[Tuple], records)
+    if isinstance(file_path_or_buffer, (str, Path)):
+        file_path_or_buffer = open(file_path_or_buffer)
 
-        for rowno, row in enumerate(rows, start=start):
-            if not row:
+    rows = csv.reader(file_path_or_buffer, delimiter=delimiter)
+    if has_headers:
+        headers = next(rows)
+        start = 1
+        records = cast(List[Dict], records)
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+    else:
+        records = cast(List[Tuple], records)
+
+    for rowno, row in enumerate(rows, start=start):
+        if not row:
+            continue
+        if indices:
+            row = [row[index] for index in indices]
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    logging.exception(f"Couldn't convert row {rowno}:{row}. {e}")
                 continue
-            if indices:
-                row = [row[index] for index in indices]
-            if types:
-                try:
-                    row = [func(val) for func, val in zip(types, row)]
-                except ValueError as e:
-                    if not silence_errors:
-                        logging.exception(f"Couldn't convert row {rowno}:{row}. {e}")
-                    continue
 
-            if has_headers:
-                records.append(dict(zip(headers, row)))
-            else:
-                records.append(tuple(row))
+        if has_headers:
+            records.append(dict(zip(headers, row)))
+        else:
+            records.append(tuple(row))
 
     return records
