05b4d568613044091aa675b7b52a54b7f03f6a1b
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-02 14:32:27 +0200

    tests(ex3.6): add another test for parse_csv with has_headers

--- tests/test_fileparse.py
+++ tests/test_fileparse.py
@@ -64,7 +64,7 @@ def test_parse_csv_select_types():
     assert portfolio == expected
 
 
-def test_parse_csv_headers():
+def test_parse_csv_noheaders():
     records = parse_csv(DATA_DIRECTORY / "prices.csv", has_headers=False)
     expected = [
         ("AA", "9.22"),
@@ -102,7 +102,7 @@ def test_parse_csv_headers():
     assert records == expected
 
 
-def test_parse_csv_types_headers():
+def test_parse_csv_types_noheaders():
     records = parse_csv(
         DATA_DIRECTORY / "prices.csv", types=[str, float], has_headers=False
     )
@@ -137,6 +137,21 @@ def test_parse_csv_types_headers():
         ("VZ", 29.26),
         ("WMT", 49.74),
         ("XOM", 69.35),
+    ]
+
+    assert records == expected
+
+
+def test_parse_csv_hasheaders():
+    records = parse_csv(DATA_DIRECTORY / "portfolio.csv", has_headers=True)
+    expected = [
+        {"name": "AA", "shares": "100", "price": "32.20"},
+        {"name": "IBM", "shares": "50", "price": "91.10"},
+        {"name": "CAT", "shares": "150", "price": "83.44"},
+        {"name": "MSFT", "shares": "200", "price": "51.23"},
+        {"name": "GE", "shares": "95", "price": "40.37"},
+        {"name": "MSFT", "shares": "50", "price": "65.10"},
+        {"name": "IBM", "shares": "100", "price": "70.44"},
     ]
 
     assert records == expected
