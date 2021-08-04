50c21e429b2f083eabce114febe899cf6f520f07
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-04 10:03:24 +0200

    feat(ex4.2): add Stock class

--- src/stock.py
+++ src/stock.py
@@ -1,9 +1,29 @@
 class Stock:
+    name: str
+    shares: int
+    price: float
+    change: float
+
     def __init__(self, name: str, shares: int, price: float) -> None:
-        pass
+        self.name = name
+        self.shares = shares
+        self.price = price
+        self.change: float
 
     def cost(self) -> float:
-        pass
+        return self.shares * self.price
 
     def sell(self, nshares: int) -> None:
-        pass
+        self.shares -= nshares
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Stock):
+            return NotImplemented
+
+        return all(
+            [
+                self.name == other.name,
+                self.shares == other.shares,
+                self.price == other.price,
+            ]
+        )
