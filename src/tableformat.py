09ebe821e76a8d3ee680c2910f5743c2ff1e26ac
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-04 15:57:50 +0200

    feat(ex4.6): add TableFormatter class

--- src/tableformat.py
+++ src/tableformat.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, cast
+import logging
 
 
 class TableFormatter:
@@ -7,7 +8,7 @@ class TableFormatter:
     This class should be extended to create tables in different formats.
     """
 
-    def headings(self, headers: List[Tuple[str, str, str, str]]) -> List[str]:
+    def headings(self, headers: Tuple[str, str, str, str]) -> List[str]:
         """
         Format the table headings.
 
@@ -17,7 +18,7 @@ class TableFormatter:
         """
         raise NotImplementedError()
 
-    def row(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
+    def rows(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
         """
         Format a single row of table data.
 
@@ -36,7 +37,15 @@ class TableFormatter:
         """
         Formats data as a table.
         """
-        pass
+        if len(data) < 2:
+            raise ValueError(
+                f"Length of data is {len(data)}. data must have headers and at least one row"
+            )
+        headers = cast(Tuple[str, str, str, str], data[0])
+        rows = cast(List[Tuple[str, int, float, float]], data[1:])
+        headings = self.headings(headers)
+        row_list = self.rows(rows)
+        return headings + row_list
 
 
 class TextTableFormatter(TableFormatter):
@@ -44,11 +53,23 @@ class TextTableFormatter(TableFormatter):
     Formats table in plain-text format.
     """
 
-    def headings(self, headers: List[Tuple[str, str, str, str]]) -> List[str]:
-        pass
+    def headings(self, headers: Tuple[str, str, str, str]) -> List[str]:
+        fmt_str = " ".join(["{:>10s}"] * len(headers))
+        dashes = " ".join(["-" * 10] * len(headers))
+        try:
+            headers_cap = list(map(lambda x: x.capitalize(), headers))
+            headers_fmt = fmt_str.format(*headers_cap)
+        except ValueError:
+            logging.exception(f"Couldn't convert headers to string: {headers}")
+        return [headers_fmt, dashes]
 
-    def row(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
-        pass
+    def rows(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
+        output: List[str] = []
+        fmt_str = " ".join(["{:>10s}"] * len(rowdata[0]))
+        for row in rowdata:
+            rowstr = [str(r) for r in row]
+            output.append(fmt_str.format(*rowstr))
+        return output
 
 
 class CSVTableFormatter(TableFormatter):
@@ -56,8 +77,13 @@ class CSVTableFormatter(TableFormatter):
     Formats table in CSV format.
     """
 
-    def headings(self, headers: List[Tuple[str, str, str, str]]) -> List[str]:
-        pass
+    def headings(self, headers: Tuple[str, str, str, str]) -> List[str]:
+        headers_cap = list(map(lambda x: x.capitalize(), headers))
+        return [",".join(headers_cap)]
 
-    def row(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
-        pass
+    def rows(self, rowdata: List[Tuple[str, int, float, float]]) -> List[str]:
+        output: List[str] = []
+        for row in rowdata:
+            rowstr = [str(r) for r in row]
+            output.append(",".join(rowstr))
+        return output
