60619d67f464b73f70c752e9a6933fab3c79bea7
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-05 15:38:28 +0200

    feat(ex6.5): add Follow class

--- src/follow.py
+++ src/follow.py
@@ -10,16 +10,19 @@ class Follow:
     file_path_or_buffer: Union[str, Path, TextIO]
 
     def __init__(self, file_path_or_buffer: Union[str, Path, TextIO]) -> None:
-        pass
+        if isinstance(file_path_or_buffer, (str, Path)):
+            file_path_or_buffer = open(file_path_or_buffer)
+        self.file_path_or_buffer = file_path_or_buffer
 
     def __iter__(self):
         """
         Return iterator of file.
         """
-        pass
+        return iter(self.file_path_or_buffer)
 
     def __next__(self):
         """
         Return next line of file.
         """
-        pass
+        line = next(self.file_path_or_buffer)
+        return line
