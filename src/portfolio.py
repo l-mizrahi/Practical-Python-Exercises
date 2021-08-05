7727a67f0f9e3de472e941fd85ed49b7bfc26691
Liron Mizrahi <l.mizrahi@instadeep.com>
2021-08-05 14:01:41 +0200

    feat(ex6.2): add Portfolio class

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,5 +1,6 @@
-from typing import Counter, List
+from typing import Iterator, List
 from .stock import Stock
+from collections import Counter
 
 
 class Portfolio:
@@ -11,20 +12,23 @@ class Portfolio:
     _holdings: List[Stock]
 
     def __init__(self, holdings: List[Stock]):
-        pass
+        self._holdings = holdings
 
-    def __iter__(self):
-        pass
+    def __iter__(self) -> Iterator[Stock]:
+        return self._holdings.__iter__()
 
     @property
     def total_cost(self) -> float:
         """
         Returns the total cost of all shares.
         """
-        pass
+        return sum([stock.shares * stock.price for stock in self._holdings])
 
     def tabulate_shares(self) -> Counter:
         """
         Returns the total count of each different share.
         """
-        pass
+        shares_counter: Counter = Counter()
+        for stock in self._holdings:
+            shares_counter[stock.name] += stock.shares
+        return shares_counter
